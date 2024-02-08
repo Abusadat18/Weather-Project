4638a421302655259c3a00ccb676ff7b995db87e
Abu Sadat Ansari <itssadatansari@gmail.com>
2024-02-09 05:28:15 +0530

    Built design layout.

---
+++ modules/home.js
@@ -0,0 +1,10 @@
+async function getWeatherData(name) {
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=211bab2228c442e69df233619240302&q=${name}`, {
+        mode: "cors"
+    })
+    const weatherInfo = await response.json();
+    console.log(response);
+    console.log(weatherInfo);
+}
+
+getWeatherData("kolkata")
